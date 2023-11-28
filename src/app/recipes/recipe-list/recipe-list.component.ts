7d2887a431b5d9da023cad8bb670266b1d0b380c
David Silva <david_goncalvessilva@hotmail.com>
2023-11-28 14:05:11 +0000

    Recipe Component - Add Recipe List and Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Ovos rotos', 'Ovos com presunto e queijo', 'https://cdn.vidaativa.pt/uploads/2020/08/receitas-ovos-rotos-850x514.jpg'),
+    new Recipe('Francesinha', 'Pão de forma com queijo e carne', 'https://img.cuisineaz.com/660x660/2022/07/18/i184751-francesinha.jpeg')
+  ];
 }
