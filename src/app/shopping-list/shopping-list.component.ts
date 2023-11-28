41367f31d75d1ec069f65d734c7cc61e2826b84b
David Silva <david_goncalvessilva@hotmail.com>
2023-11-28 14:14:20 +0000

    Creation of the ingredient Model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Ovos', 12),
+    new Ingredient('Presunto', 10),
+    new Ingredient('Queijo', 200)
+  ];
 }
