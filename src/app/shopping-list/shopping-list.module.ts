c4317787d9dee0e232c17ca2ed61fa944c592a94
David Silva <david_goncalvessilva@hotmail.com>
2024-01-09 12:25:24 +0000

    Splitting Modules - Shared Modules

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,21 +1,21 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
         ShoppingListComponent,
-        ShoppingEditComponent,
+        ShoppingEditComponent
     ],
     imports: [
-        CommonModule,
         RouterModule,
         FormsModule,
-        ShoppingListRoutingModule
+        ShoppingListRoutingModule,
+        SharedModule
     ]
 })
 export class ShoppingListModule { }
